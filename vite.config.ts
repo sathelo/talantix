c5e19e6d14621652d2b0fac0254bed314abc7f79
sathelo <sathelo@yandex.ru>
2024-03-13 22:53:00 +0300

    fix: Исправил ошибку с доступом .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,27 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    server: {
-      host: "localhost",
-      port: 8080,
-      strictPort: true,
+export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 8080,
+    strictPort: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
             @import "./src/assets/styles/global.scss";
           `,
-        },
       },
     },
-  });
-};
+  },
+});
